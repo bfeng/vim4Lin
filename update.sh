b94ecb8a8b5df246ccf5251663d489311e1eed07
Bo Feng <bfeng1216@gmail.com>
2013-02-17 01:49:05 -0600

    fixed a few bugs and updated submodules

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git submodule init
 git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
-#git submodule foreach git pull origin master
+git submodule foreach git pull origin master
 
 # special case
 # vim-powerline : develop
