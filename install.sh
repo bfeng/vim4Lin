ab4559a5ad1e253431172342773983184938e324
Bo Feng <bfeng1216@gmail.com>
2014-10-13 11:34:47 -0500

    added screenshots

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
-cd ~
-ln -s vim4Lin .vim
-ln -s .vim/vimrc .vimrc
-ln -s .vim/gvimrc .gvimrc
+#!/bin/sh
+
+homedir="$(getent passwd $USER | awk -F ':' '{print $6}')"
+
+if [ ! -h ${homedir}/.vim ]
+then
+  ln -s ${homedir}/vim4Lin ${homedir}/.vim
+  ln -s ${homedir}/.vim/vimrc ${homedir}/.vimrc
+  ln -s ${homedir}/.vim/gvimrc ${homedir}/.gvimrc
+fi
